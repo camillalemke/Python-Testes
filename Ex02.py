7dd268ff8d00945e4300af5b7954210309249f61
Camilla Santana <159941222+camillalemke@users.noreply.github.com>
2024-02-14 17:25:38 -0300

    Ex02.py

--- Ex02.py
+++ Ex02.py
@@ -1,3 +1,5 @@
+# Criar um programa que leia o preço de um produto e depois mostre o novo preço com desconto de 5%
+
 preco = float(input('Digite preço R$: '))
 novo = preco * 5/100
 print('O preco do produto é R$ {}, com desconto de 5% ficou R$ {} '.format(preco, novo) )
