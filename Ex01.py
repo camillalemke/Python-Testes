7601af3c5cf27938a5ea8973d0e7d20dc1a5f380
Camilla Santana <159941222+camillalemke@users.noreply.github.com>
2024-02-14 17:23:45 -0300

    Ex01.py

--- Ex01.py
+++ Ex01.py
@@ -2,4 +2,7 @@ larg = float(input('Largura da parede '))
 alt = float(input('Altura da parede '))
 area = larg * alt
 print('A parede tem {} x {} que são iguais a {} '.format(larg, alt, area))
-tinta =
+tinta = area / 2
+print('A quantidade de tinta para pintar {} m é de {} '.format(tinta))
+
+
