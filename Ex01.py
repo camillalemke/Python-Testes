32c8e3e976902797cf76218834f443abab9e23c6
Camilla Santana <159941222+camillalemke@users.noreply.github.com>
2024-02-20 15:47:32 -0300

    Update Ex01.py

--- Ex01.py
+++ Ex01.py
@@ -1,3 +1,7 @@
+# Criar um programa que leia a largura e altura de uma parede
+# Calcular a área da parede e quantidade de tinta para pintar cada 2 metros quadrados 
+
+
 larg = float(input('Largura da parede '))
 alt = float(input('Altura da parede '))
 area = larg * alt
