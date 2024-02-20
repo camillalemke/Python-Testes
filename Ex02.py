903d7d2c841b13b79477f22d9ae57a7594e7d2bb
Camilla Santana <159941222+camillalemke@users.noreply.github.com>
2024-02-20 15:48:17 -0300

    Update Ex02.py

--- Ex02.py
+++ Ex02.py
@@ -2,4 +2,4 @@
 
 preco = float(input('Digite preço R$: '))
 novo = preco * 5/100
-print('O preco do produto é R$ {}, com desconto de 5% ficou R$ {} '.format(preco, novo) )
+print('Preco do produto é R$ {}, com desconto de 5% ficou R$ {} '.format(preco, novo))
